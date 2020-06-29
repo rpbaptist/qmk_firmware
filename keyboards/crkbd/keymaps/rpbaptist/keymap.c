bea9193f73ef72f760fb299c1df53c7127ed10b4
Richard Baptist <rpbaptist@gmail.com>
2020-06-29 09:28:09 +0200

    Less reduction in tapping term for enter

--- keyboards/crkbd/keymaps/rpbaptist/keymap.c
+++ keyboards/crkbd/keymaps/rpbaptist/keymap.c
@@ -465,7 +465,7 @@ void suspend_wakeup_init_keymap(void) {
 uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
         case SFT_ENT:
-            return TAPPING_TERM - 30;
+            return TAPPING_TERM - 20;
         default:
             return TAPPING_TERM;
     }
