ee2553599d53031afef218a18928c3a64ae15811
Richard Baptist <rpbaptist@gmail.com>
2020-08-18 15:44:55 +0200

    Replace link for online editor

--- keyboards/crkbd/keymaps/rpbaptist/glcdfont_pulse.c
+++ keyboards/crkbd/keymaps/rpbaptist/glcdfont_pulse.c
@@ -1,7 +1,7 @@
 #include "progmem.h"
 
 // Helidox 8x6 font with QMK Firmware Logo
-// Online editor: http://teripom.x0.com/
+// Online editor: https://joric.github.io/qle/
 
 const unsigned char font[] PROGMEM = {
   0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
