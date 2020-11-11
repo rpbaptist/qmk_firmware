08a983e2c18ec118dcd0f17ec985668ef2be62ca
Richard Baptist <rpbaptist@gmail.com>
2020-11-11 11:48:35 +0100

    Reduce max brightness

--- keyboards/crkbd/keymaps/rpbaptist/config.h
+++ keyboards/crkbd/keymaps/rpbaptist/config.h
@@ -45,7 +45,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #ifdef RGB_MATRIX_ENABLE
 #    define RGB_DISABLE_WHEN_USB_SUSPENDED true  // turn off effects when suspended
-#    define RGB_MATRIX_MAXIMUM_BRIGHTNESS 150
+#    define RGB_MATRIX_MAXIMUM_BRIGHTNESS 135
 
 #    define RGB_MATRIX_HUE_STEP 8
 #    define RGB_MATRIX_SAT_STEP 12
