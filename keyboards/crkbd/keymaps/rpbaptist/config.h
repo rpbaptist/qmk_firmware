8f45af499194f9be7c4e7ed8861e4f71070350b8
Richard Baptist <code@richardbaptist.nl>
2021-06-07 11:47:19 +0200

    Revert "Debugging values"
    This reverts commit 35c6ac9fb103925b330297faa444ec16c8504a69.
    
    ; Conflicts:
    ;	keyboards/crkbd/keymaps/rpbaptist/config.h

--- keyboards/crkbd/keymaps/rpbaptist/config.h
+++ keyboards/crkbd/keymaps/rpbaptist/config.h
@@ -28,7 +28,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define USB_POLLING_INTERVAL_MS 1
 
 #ifdef OLED_DRIVER_ENABLE
-#    define OLED_TIMEOUT 30000
+#    define OLED_TIMEOUT 600000
 #endif
 
 #undef USE_I2C
@@ -43,8 +43,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define NO_ACTION_ONESHOT
 
 #ifdef RGB_MATRIX_ENABLE
-#    define IDLE_TIMEOUT 5000
-// #    define IDLE_TIMEOUT 60000 * 5
+#    define IDLE_TIMEOUT 60000 * 5
 #    define GAMING_IDLE_TIMEOUT 10000
 #    define RGB_MATRIX_TYPING_ACTIVE RGB_MATRIX_SOLID_REACTIVE_SIMPLE
 #    define RGB_MATRIX_TYPING_PASSIVE RGB_MATRIX_CYCLE_ALL
